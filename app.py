c2ca592306b5a2c1e35895476d335ed4d01ac9e2
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2024-10-05 23:50:16 +0000

    h

--- app.py
+++ app.py
@@ -9,29 +9,26 @@ app = Flask(__name__)
 key = 'AIzaSyDszwHyMkqcPjJtH6Qn2cpoPcGzzqYgu3E'
 
 
-
 @app.route('/')
 def index():
-    datas = video_url()
-    data = []
-    index = "0123456789"
-
-    for i in range(0,6):
-      img = datas[i]["snippet"]["thumbnails"]['default']['url']
-      video_title = datas[i]['snippet']['title']
-      channel_title = datas[i]['snippet']['channelTitle']
-      data.append([img,video_title,channel_title])
-      print(data)
-
-    video_id = 'mqhxxeeTbu0'
-    video_link = f'https://youtube.com/watch?v={video_id}'
-    return render_template('home.html', video_link = video_link,data=data)
+      datas = video_url()[0]['snippet']
+      img = datas["thumbnails"]['default']['url']
+      video_title = datas['title']
+      channel_title = datas['channelTitle']
+      channel_id = datas['channelId']
+      video_id = '_kZvTSwBkwE'
+      channel_req = f'https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,statistics&channelId={channel_id}&key={key}'
+      res = requests.get(channel_req)
+      channel = res.json()
+      video_link = f'https://youtube.com/watch?v={video_id}'
+      
+      return render_template('home.html', video_link = video_link,img = img, video_title = video_title, channel_title = channel_title,  datas = datas,channel = channel)
 
 
 
 def video_url():
-   # youtube_api_url = f'https://www.googleapis.com/youtube/v3/videos?id={video_id}&key={key}&part=snippet,contentDetails,statistics'
-    youtube_api_url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=9&q=flask+tuto&key={key}'
+    youtube_api_url = f'https://www.googleapis.com/youtube/v3/videos?id=_kZvTSwBkwE&key={key}&part=snippet,contentDetails,statistics'
+    #youtube_api_url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResult=9&q=flask+tuto&key={key}'
     response = requests.get(youtube_api_url)
     if response.status_code == 200:
         data = response.json()
